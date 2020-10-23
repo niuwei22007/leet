42ced95a998a56bdf6d3e33ca769a02515efd2e4
niuwei22007 <371073260@qq.com>
2020-10-23 23:42:58 +0800

    finish 78

--- search/combine/combine_78.cpp
+++ search/combine/combine_78.cpp
@@ -46,7 +46,7 @@ public:
         }
         nums_ = nums;
         sort(nums_.begin(), nums_.end());
-        size_t size = (size_t)1 << nums_.size();
+        size_t size = (size_t) 1 << nums_.size();
         result_.reserve(size);
         int totalLen = nums_.size();
         for (int i = 1; i <= totalLen; i++) {
@@ -54,13 +54,41 @@ public:
         }
         return result_;
     }
+
+    vector<vector<int>> subsets2(vector<int> &nums) {
+        //子集的长度是2的nums.length次方，这里通过移位计算
+        int length = (int) 1 << nums.size();
+        vector<vector<int>> res(length);
+        //遍历从0到length中间的所有数字，根据数字中1的位置来找子集
+        for (int i = 0; i < length; i++) {
+            vector<int> list;
+            for (int j = 0; j < nums.size(); j++) {
+                //如果数字i的某一个位置是1，就把数组中对
+                //应的数字添加到集合
+                printf("i=%d(%x), j=%d(%x), i>>j=%d, &1=%d\n", i, i, j, j, i >> j, (i >> j) & 1);
+                if (((i >> j) & 1) == 1) {
+                    list.push_back(nums[j]);
+                }
+            }
+            res.push_back(list);
+        }
+        return res;
+    }
 };
 }
 
 void TestForCombine78() {
     auto *obj = new combine78::Solution();
-    vector<int> nums{3, 9};
+    vector<int> nums{1, 2, 3};
     auto result = obj->subsets(nums);
+    for (const auto &r:result) {
+        for (auto c : r) {
+            printf("%d ", c);
+        }
+        printf("\n");
+    }
+
+    auto result2 = obj->subsets2(nums);
     for (const auto &r:result) {
         for (auto c : r) {
             printf("%d ", c);
