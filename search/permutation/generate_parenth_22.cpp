369cb735c3145378a87425349d71bf9ac259bef3
niuwei22007 <371073260@qq.com>
2020-10-30 22:23:17 +0800

    finish 22 with DP

--- search/permutation/generate_parenth_22.cpp
+++ search/permutation/generate_parenth_22.cpp
@@ -11,6 +11,28 @@
 using namespace std;
 namespace generate_parenth {
 class Solution {
+    vector<vector<string>> dp_;
+public:
+    vector<string> generateParenthesis(int n) {
+        if (n == 0) {
+            return vector<string>();
+        }
+        dp_ = vector<vector<string>>(n + 1, vector<string>());
+        dp_[0].push_back("");
+        for (int i = 1; i <= n; i++) {
+            for (int j = 0; j < i; j++) {
+                for (const auto &q : dp_[j]) {
+                    for (const auto &p : dp_[i - 1 - j]) {
+                        dp_[i].push_back("(" + q + ")" + p);
+                    }
+                }
+            }
+        }
+        return dp_[n];
+    }
+};
+
+class Solution_DFS {
     vector<string> res_;
 public:
     void DFS(int target, int left, int state, string &context) {
@@ -47,7 +69,7 @@ public:
 
 void TestForGenerateParenth() {
     auto *obj = new generate_parenth::Solution();
-    auto result = obj->generateParenthesis(8);
+    auto result = obj->generateParenthesis(3);
     for (auto &s : result) {
         printf("%s\n", s.c_str());
     }
