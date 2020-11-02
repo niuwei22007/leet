fe121a29b18f96697896b9694a7c2a2a6fe9e64c
niuwei22007 <371073260@qq.com>
2020-11-02 23:21:36 +0800

    finish 37 with bitset

--- search/dfs/sudoku_solver_37.cpp
+++ search/dfs/sudoku_solver_37.cpp
@@ -8,12 +8,87 @@
 #include <sstream>
 #include <map>
 #include <set>
+#include <bitset>
 #include <unordered_set>
 #include <unordered_map>
 
 using namespace std;
 namespace sudoku_solver {
 class Solution {
+    vector<bitset<9>> rows_;
+    vector<bitset<9>> cols_;
+    vector<vector<bitset<9>>> ceils_;
+public:
+
+    void FillDigit(int i, int j, int digit, bool isSet) {
+        rows_[i].set(digit, isSet);
+        cols_[j].set(digit, isSet);
+        ceils_[i / 3][j / 3].set(digit, isSet);
+    }
+
+    pair<pair<int, int>, bitset<9>> GetNext(vector<vector<char>> &board) {
+        int bestRow = 0, bestCol = 0;
+        bitset<9> bestValue(0);
+        bitset<9> tmp;
+        for (int i = 0; i < board.size(); i++) {
+            for (int j = 0; j < board[0].size(); j++) {
+                if (board[i][j] != '.') {
+                    continue;
+                }
+                tmp = rows_[i] | cols_[j] | ceils_[i / 3][j / 3];
+                if (tmp.count() > bestValue.count()) {
+                    bestValue = tmp;
+                    bestRow = i;
+                    bestCol = j;
+                }
+            }
+        }
+        return make_pair(make_pair(bestRow, bestCol), bestValue);
+    }
+
+    bool DFS(vector<vector<char>> &board, int empty) {
+        if (empty == 0) {
+            return true;
+        }
+        auto next = GetNext(board);
+        int row = next.first.first;
+        int col = next.first.second;
+        bitset<9> value = next.second;
+
+        for (int i = 0; i < 9; i++) {
+            if (!value[i]) {
+                board[row][col] = i + '1';
+                FillDigit(row, col, i, true);
+                if (DFS(board, empty - 1)) {
+                    return true;
+                }
+                FillDigit(row, col, i, false);
+                board[row][col] = '.';
+            }
+        }
+        return false;
+    }
+
+    void solveSudoku(vector<vector<char>> &board) {
+        rows_ = vector<bitset<9>>(9, bitset<9>(0));
+        cols_ = vector<bitset<9>>(9, bitset<9>(0));
+        ceils_ = vector<vector<bitset<9>>>(3, vector<bitset<9>>(3, bitset<9>(0)));
+        int empty = 0;
+        for (int i = 0; i < board.size(); i++) {
+            for (int j = 0; j < board[0].size(); j++) {
+                if (board[i][j] == '.') {
+                    empty++;
+                } else {
+                    int digit = board[i][j] - '1';
+                    FillDigit(i, j, digit, true);
+                }
+            }
+        }
+        DFS(board, empty);
+    }
+};
+
+class Solution_Common {
     bool rows_[9][9] = {false};
     bool cols_[9][9] = {false};
     bool grid_[3][3][9] = {false};
