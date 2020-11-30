1feee5a8795aaf4270f5e72ccce7718f36ae3c33
tiger <371073260@qq.com>
2020-11-30 22:58:15 +0800

    format code

--- multi_tree/segment_tree/part_search_307.cpp
+++ multi_tree/segment_tree/part_search_307.cpp
@@ -9,83 +9,82 @@
 
 using namespace std;
 namespace partsearch {
-    class NumArray {
-        vector<int> tree_;
-        int size_;
-    public:
-        NumArray(vector<int> &nums) {
-            size_ = nums.size();
-            tree_.resize(size_ * 2);
-            BuildTree(nums);
+class NumArray {
+    vector<int> tree_;
+    int size_;
+public:
+    NumArray(vector<int>& nums) {
+        size_ = nums.size();
+        tree_.resize(size_ * 2);
+        BuildTree(nums);
+    }
+
+    static inline int LeftChild(int i) {
+        return i * 2;
+    }
+
+    static inline int IsLeft(int i) {
+        return i % 2 == 0;
+    }
+
+    static inline int RightChild(int i) {
+        return i * 2 + 1;
+    }
+
+    static inline int IsRight(int i) {
+        return i % 2 == 1;
+    }
+
+    static inline int ParentIndex(int i) {
+        return i / 2;
+    }
+
+    inline int TreeIndex(int i) const {
+        return i + size_;
+    }
+
+    void BuildTree(vector<int>& nums) {
+        for (int i = nums.size(), j = 0; i < tree_.size(); ++i, ++j) {
+            tree_[i] = nums[j];
         }
-
-        static inline int LeftChild(int i) {
-            return i * 2;
+        for (int i = nums.size() - 1; i > 0; i--) {
+            tree_[i] = tree_[LeftChild(i)] + tree_[RightChild(i)];
         }
+    }
 
-        static inline int IsLeft(int i) {
-            return i % 2 == 0;
+    void update(int i, int val) {
+        i = TreeIndex(i);
+        tree_[i] = val;
+        while (i > 0) {
+            i = ParentIndex(i);
+            tree_[i] = tree_[LeftChild(i)] + tree_[RightChild(i)];
         }
+    }
 
-        static inline int RightChild(int i) {
-            return i * 2 + 1;
-        }
-
-        static inline int IsRight(int i) {
-            return i % 2 == 1;
-        }
-
-        static inline int ParentIndex(int i) {
-            return i / 2;
-        }
-
-
-        inline int TreeIndex(int i) const {
-            return i + size_;
-        }
-
-        void BuildTree(vector<int> &nums) {
-            for (int i = nums.size(), j = 0; i < tree_.size(); ++i, ++j) {
-                tree_[i] = nums[j];
+    int sumRange(int i, int j) {
+        int left = TreeIndex(i);
+        int right = TreeIndex(j);
+        int sum = 0;
+        while (left <= right) {
+            if (IsRight(left)) {
+                sum += tree_[left];
+                left++;
             }
-            for (int i = nums.size() - 1; i > 0; i--) {
-                tree_[i] = tree_[LeftChild(i)] + tree_[RightChild(i)];
+            if (IsLeft(right)) {
+                sum += tree_[right];
+                right--;
             }
+            left = ParentIndex(left);
+            right = ParentIndex(right);
         }
-
-        void update(int i, int val) {
-            i = TreeIndex(i);
-            tree_[i] = val;
-            while (i > 0) {
-                i = ParentIndex(i);
-                tree_[i] = tree_[LeftChild(i)] + tree_[RightChild(i)];
-            }
-        }
-
-        int sumRange(int i, int j) {
-            int left = TreeIndex(i);
-            int right = TreeIndex(j);
-            int sum = 0;
-            while (left <= right) {
-                if (IsRight(left)) {
-                    sum += tree_[left];
-                    left++;
-                }
-                if (IsLeft(right)) {
-                    sum += tree_[right];
-                    right--;
-                }
-                left = ParentIndex(left);
-                right = ParentIndex(right);
-            }
-            return sum;
-        }
-    };
+        return sum;
+    }
+};
 }
 
 void TestForPartSearch() {
     vector<int> list = {1, 2, 3, 4};
-    auto *obj = new partsearch::NumArray(list);
+    auto* obj = new partsearch::NumArray(list);
     auto result = obj->sumRange(0, 2);
     printf("Result: %d|\n", result);
 
