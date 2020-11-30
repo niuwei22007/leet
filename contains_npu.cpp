9c887d1989df2b0134007bcd851bf216c9421b47
tiger <371073260@qq.com>
2020-11-30 20:12:28 +0800

    delete unused file

--- contains_npu.cpp
+++
@@ -1,8 +0,0 @@
-#include <istream>
-#include <string>
-#include <vector>
-#include <array>
-//#include <sys/system_properties.h>
-
-static std::vector<std::string> g_npuList{"kirin990", "kirin810"};
-
