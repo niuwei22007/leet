17dde5d9d322b7be10796e09c5b2221c1880adf1
tiger <371073260@qq.com>
2020-12-02 21:04:40 +0800

    finish 34 with binarySearch

--- dp/find_begin_end_34.cpp
+++ dp/find_begin_end_34.cpp
@@ -14,22 +14,43 @@ using namespace std;
 namespace find_begin_end {
 class Solution {
 public:
+    int BinarySearch(vector<int>& nums, int target, bool isLeft) {
+        if (nums.empty()) {
+            return -1;
+        }
+        int left = 0;
+        int right = nums.size() - 1;
+        int mid;
+        int ans = nums.size();
+        while (left <= right) {
+            mid = (left + right) >> 1;
+            if (nums[mid] > target || (isLeft && nums[mid] >= target)) {
+                right = mid - 1;
+                ans = mid;
+            } else {
+                left = mid + 1;
+            }
+        }
+        return ans;
+    }
+
     vector<int> searchRange(vector<int>& nums, int target) {
         if (nums.empty() || nums[0] > target || nums[nums.size() - 1] < target) {
             return {-1, -1};
         }
-        auto[b, e] = equal_range(nums.begin(), nums.end(), target);
-        if (b == e) {
-            return {-1, -1};
+        int b = BinarySearch(nums, target, true);
+        int e = BinarySearch(nums, target, false) - 1;
+        if (b <= e && e < nums.size() && nums[b] == target && nums[e] == target) {
+            return {b, e};
         }
-        return {(int)distance(nums.begin(), b), (int)distance(nums.begin(), e) - 1};
+        return {-1, -1};
     }
 };
 }
 
 void TestForFindBeginEnd() {
-    vector<int> cost{5, 7, 7, 8, 8, 10};
+    vector<int> cost{1, 2, 3};
     auto* obj = new find_begin_end::Solution();
-    auto res = obj->searchRange(cost, 8);
+    auto res = obj->searchRange(cost, 1);
     printf("%d, %d\n", res[0], res[1]);
 }
