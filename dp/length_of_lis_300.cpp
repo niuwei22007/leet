f2efcbbe73f019156144f6f23b52dcd488050522
tiger <371073260@qq.com>
2020-12-18 21:34:38 +0800

    finish 300 with DP&Bisearch

--- dp/length_of_lis_300.cpp
+++ dp/length_of_lis_300.cpp
@@ -15,7 +15,7 @@ using namespace std;
 namespace length_of_lis {
 class Solution {
 public:
-    int lengthOfLIS(vector<int>& nums) {
+    int lengthOfLIS_DP(vector<int>& nums) {
         vector<int> dp(nums.size(), 1);
         for (int i = 0; i < nums.size(); i++) {
             for (int j = i - 1; j >= 0; j--) {
@@ -26,12 +26,36 @@ public:
         }
         return *max_element(dp.begin(), dp.end());
     }
+
+    int lengthOfLIS(vector<int>& nums) {
+        vector<int> dp;
+        dp.push_back(nums[0]);
+        for (int i = 1; i < nums.size(); i++) {
+            if (nums[i] > dp.back()) {
+                dp.push_back(nums[i]);
+            } else {
+                int l = 0;
+                int r = dp.size() - 1;
+                int mid = (l + r) / 2;
+                while (l < r) {
+                    if (dp[mid] >= nums[i]) {
+                        r = mid;
+                    } else {
+                        l = mid + 1;
+                    }
+                    mid = (l + r) / 2;
+                }
+                dp[mid] = nums[i];
+            }
+        }
+        return dp.size();
+    }
 };
 }
 
 void TestForMaxLengthSubSeq() {
     auto* obj = new length_of_lis::Solution();
-    vector<int> dict{1, 3, 6, 7, 9, 4, 10, 5, 6};
+    vector<int> dict{10, 9, 2, 5, 3, 7, 101, 18};
 
     auto res = obj->lengthOfLIS(dict);
     printf("%d\n", res);
