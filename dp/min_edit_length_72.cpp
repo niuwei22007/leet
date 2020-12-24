32cec0fba7b27c21207593a304d5d02959206a5a
tiger <371073260@qq.com>
2020-12-25 00:19:40 +0800

    finish 72 with DP

--- dp/min_edit_length_72.cpp
+++ dp/min_edit_length_72.cpp
@@ -15,7 +15,7 @@ using namespace std;
 namespace min_edit_length {
 class Solution {
 public:
-    int minDistance(string& word1, int i1, string& word2, int i2, vector<vector<int>>& memo) {
+    int minDistance_DFS(string& word1, int i1, string& word2, int i2, vector<vector<int>>& memo) {
         if (i1 == -1 || i2 == -1) {
             return max(i1, i2) + 1;
         }
@@ -33,9 +33,31 @@ public:
         return memo[i1][i2];
     }
 
-    int minDistance(string word1, string word2) {
+    int minDistance_DFS(string word1, string word2) {
         vector<vector<int>> memo(word1.size(), vector<int>(word2.size(), -1));
-        return minDistance(word1, word1.size() - 1, word2, word2.size() - 1, memo);
+        return minDistance_DFS(word1, word1.size() - 1, word2, word2.size() - 1, memo);
+    }
+
+    int minDistance(string word1, string word2) {
+        int n = word1.size();
+        int m = word2.size();
+        int dp[n + 1][m + 1];
+        for (int i = 0; i < n + 1; i++) {
+            dp[i][0] = i;
+        }
+        for (int i = 0; i < m + 1; i++) {
+            dp[0][i] = i;
+        }
+        for (int i = 1; i < n + 1; i++) {
+            for (int j = 1; j < m + 1; j++) {
+                if (word1[i - 1] == word2[j - 1]) {
+                    dp[i][j] = dp[i - 1][j - 1];
+                } else {
+                    dp[i][j] = 1 + min(min(dp[i][j - 1], dp[i - 1][j]), dp[i - 1][j - 1]);
+                }
+            }
+        }
+        return dp[n][m];
     }
 };
 }
